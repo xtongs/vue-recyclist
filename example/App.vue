bacfdfdbba44bd273150f4069527469a447fca77
Tony Shen <tony.shen@vipshop.com>
2017-02-23 17:58:41 +0800

    fill dom

--- example/App.vue
+++ example/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
-    <h1>Infinite scroll component for Vue.js with DOM recycling.</h1>
-    <vue-recyclist></vue-recyclist>
+    <header>
+      <h1>VueRecyclist</h1>
+      <h2>Infinite scroll list for Vue.js with DOM recycling.</h2>
+    </header>
+    <vue-recyclist class="list" :items="list">
+      <div slot="tombstone">tombstone</div>
+
+      <template slot="item" scope="props">
+        <div class="li" @click="itemClicked(props)">{{ props.index }} - {{props.data}}</div>
+      </template>
+
+      <div slot="spinner">spinner</div>
+    </vue-recyclist>
   </div>
 </template>
 
@@ -11,44 +22,90 @@ export default {
   name: 'app',
   data () {
     return {
-
+      list: new Array(100).join(0).split('')
     }
   },
   components: {
     'vue-recyclist': VueRecyclist
+  },
+  mounted () {
+
+  },
+  methods: {
+    itemClicked (props) {
+      console.log(props)
+    }
   }
 }
 </script>
 
 <style lang="scss">
-body {
-  margin: 0;
-  padding: 0;
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  overflow: hidden;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  html {
+    height: 100%;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      overflow: hidden;
+      padding: 100px 20px 50px;
+      width: 100%;
+      height: 100%;
+      box-sizing: border-box;
+      background: #f3f4f5;
+      header {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100px;
+        padding: 10px 0;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        box-sizing: border-box;
+        h1, h2 {
+          font-weight: normal;
+          margin: 0;
+        }
+        h1 {
+          font-size: 24px;
+        }
+        h2 {
+          font-size: 14px;
+        }
+      }
+    }
+  }
+</style>
+<style lang="scss" scoped>
+  .list {
+    width: 375px;
+    max-width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    padding: 0;
+    border: 1px solid #eee;
+    background: #fff;
+    list-style-type: none;
+    text-align: center;
+    .li {
+      display: block;
+      height: 50px;
+      line-height: 50px;
+      padding: 0 10px;
+      text-align: left;
+      & + .li {
+        border-top: 1px solid #eee;
+      }
+    }
+  }
 </style>
--- src/components/VueRecyclist.vue
+++ src/components/VueRecyclist.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="vue-recyclist">
-
+    <slot name="tombstone"></slot>
+    <div class="vue-recyclist-items">
+      <slot name="item" v-for="(item, index) in items" :data="item" :index="index"></slot>
+    </div>
+    <slot name="spinner"></slot>
   </div>
 </template>
 <script>
   export default {
+    props: ['items'],
     data() {
       return {
-
+        name: 'VueRecyclist'
       }
     },
     computed: {
-
-    },
-    props: {
 
     },
     mounted () {
@@ -28,5 +30,13 @@
   }
 </script>
 <style lang="scss" scoped>
-
+  .vue-recyclist {
+    overflow-x: hidden;
+    overflow-y: auto;
+    -webkit-overflow-scrolling: touch;
+    .vue-recyclist-items {
+      margin: 0;
+      padding: 0;
+    }
+  }
 </style>
