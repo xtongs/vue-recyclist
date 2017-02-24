0c82fa28a213f56b6cf4d638cfbaf4b8862a09dd
Tony Shen <tony.shen@vipshop.com>
2017-02-24 15:29:43 +0800

    update demo page list style

--- example/App.vue
+++ example/App.vue
@@ -5,24 +5,46 @@
       <h2>Infinite scroll list for Vue.js with DOM recycling.</h2>
     </header>
     <vue-recyclist class="list" :items="list">
-      <div slot="tombstone">tombstone</div>
+      <div slot="tombstone" class="item tombstone">
+        <img class="avatar" src="./images/unknown.jpg"/>
+        <div class="bubble">
+          <p></p>
+          <p></p>
+          <p></p>
+          <div class="meta">
+            <time class="posted-date"></time>
+          </div>
+        </div>
+      </div>
 
       <template slot="item" scope="props">
-        <div class="li" @click="itemClicked(props)">{{ props.index }} - {{props.data}}</div>
+        <div class="item" @click="itemClicked(props)">
+          <img class="avatar" src="./images/unknown.jpg"/>
+          <div class="bubble">
+            <p></p>
+            <p></p>
+            <p></p>
+            <div class="meta">
+              <time class="posted-date"></time>
+            </div>
+          </div>
+        </div>
       </template>
 
-      <div slot="spinner">spinner</div>
+      <!--<div slot="loading">spinner</div>-->
     </vue-recyclist>
   </div>
 </template>
 
 <script>
+import Data from './data'
 import VueRecyclist from '../src/index'
 export default {
   name: 'app',
   data () {
     return {
-      list: new Array(100).join(0).split('')
+      list: new Array(10).join(0).split(''),
+      data: Data
     }
   },
   components: {
@@ -58,7 +80,7 @@ export default {
       width: 100%;
       height: 100%;
       box-sizing: border-box;
-      background: #f3f4f5;
+      background: #fff;
       header {
         position: absolute;
         top: 0;
@@ -83,6 +105,9 @@ export default {
           font-size: 14px;
         }
       }
+      .cssloading-circle {
+        background: #eee;
+      }
     }
   }
 </style>
@@ -93,18 +118,60 @@ export default {
     height: 100%;
     margin: 0 auto;
     padding: 0;
-    border: 1px solid #eee;
+    border: 1px solid #ddd;
     background: #fff;
     list-style-type: none;
     text-align: center;
-    .li {
-      display: block;
-      height: 50px;
-      line-height: 50px;
-      padding: 0 10px;
-      text-align: left;
-      & + .li {
-        border-top: 1px solid #eee;
+    background: #eee;
+    .item {
+      display: flex;
+      padding: 10px 0;
+      width: 100%;
+      will-change: transform;
+      .avatar {
+        border-radius: 500px;
+        margin-left: 20px;
+        margin-right: 6px;
+        min-width: 48px;
+        width: 48px;
+        height: 48px;
+      }
+      p {
+        margin: 0;
+        word-wrap: break-word;
+        font-size: 14px;
+      }
+      &.tombstone {
+        p {
+          width: 100%;
+          height: 0.5em;
+          background-color: #ccc;
+          margin: 0.5em 0;
+        }
+      }
+      .bubble {
+        padding: 7px 10px;
+        color: #333;
+        background: #fff;
+        box-shadow: 0 3px 2px rgba(0,0,0,0.1);
+        position: relative;
+        max-width: 420px;
+        min-width: 80px;
+        margin: 0 5px;
+        &:before {
+          content: '';
+          border-style: solid;
+          border-width: 0 10px 10px 0;
+          border-color: transparent #fff transparent transparent;
+          position: absolute;
+          top: 0;
+          left: -10px;
+        }
+      }
+      .meta {
+        font-size: 0.8rem;
+        color: #999;
+        margin-top: 3px;
       }
     }
   }
--- src/components/VueRecyclist.vue
+++ src/components/VueRecyclist.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="vue-recyclist">
-    <slot name="tombstone"></slot>
     <div class="vue-recyclist-items">
-      <slot name="item" v-for="(item, index) in items" :data="item" :index="index"></slot>
+      <div class="vue-recyclist-item vue-recyclist-tomb" v-for="t in tombs">
+        <slot name="tombstone"></slot>
+      </div>
+      <div class="vue-recyclist-item" v-for="(item, index) in items">
+        <slot name="item" :data="item" :index="index"></slot>
+      </div>
+      <div class="vue-recyclist-item vue-recyclist-tomb" v-for="t in tombs">
+        <slot name="tombstone"></slot>
+      </div>
     </div>
-    <slot name="spinner"></slot>
+
+    <div ref="loading" class="vue-recyclist-loading" v-show="loading && showLoading">
+      <slot name="loading">
+        <div class="vue-recyclist-loading-content">
+          <div class="cssloading-circle spinner"></div>
+        </div>
+      </slot>
+    </div>
   </div>
 </template>
 <script>
   export default {
-    props: ['items'],
-    data() {
+    data () {
       return {
-        name: 'VueRecyclist'
+        name: 'VueRecyclist',
+        showLoading: true
+      }
+    },
+    props: {
+      tombstone: {
+        type: Boolean,
+        default: true // Whether to show tombstones.
+      },
+      tombs: {
+        type: Number,
+        default: 10 // Number of tombstones beyond current view in both scroll directions.
+      },
+      offset: {
+        type: Number,
+        default: 2000 // The number of pixels of additional length to allow scrolling to.
+      },
+      duration: {
+        type: Number,
+        default: 200 // The animation interval (in ms) for fading in content from tombstones.
+      },
+      loading: {
+        type: Boolean,
+        default: true // Whether to show loading status bar.
+      },
+      items: {
+        type: Array,
+        default: () => []
       }
     },
     computed: {
@@ -29,6 +69,7 @@
     }
   }
 </script>
+<style src="./cssloading.css"></style>
 <style lang="scss" scoped>
   .vue-recyclist {
     overflow-x: hidden;
@@ -37,6 +78,17 @@
     .vue-recyclist-items {
       margin: 0;
       padding: 0;
+    }
+    .vue-recyclist-loading {
+      .vue-recyclist-loading-content {
+        width: 100%;
+        text-align: center;
+        .spinner {
+          margin: 10px auto;
+          width: 20px;
+          height: 20px;
+        }
+      }
     }
   }
 </style>
